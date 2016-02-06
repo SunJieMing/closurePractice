47490ab30faa052c51c81bbe0525cb41339ba67b
Ben Nelson <benn@ifit.com>
2016-02-06 04:17:47 -0700

    Completed working solutions

--- practice.js
+++ practice.js
@@ -4,16 +4,16 @@ var outer = function(){
   var name = 'Tyler';
   return function(){
     return 'The original name was ' + name;
-  }
-}
+  };
+};
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
-  //Code Here
+var inner = outer();
 
 //Once you do that, invoke inner.
 
-  //Code Here
+inner();
 
 
 
@@ -32,7 +32,30 @@ var callFriend = function(){
 //Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
-  //Code Here
+var callJake = callFriend();
+callJake('435-215-9248');
+
+
+//Next Problem
+
+
+
+/*
+Write a function called makeCounter that makes the following code work properly.
+*/
+
+var makeCounter = function() {
+  var count = 0;
+  return function() {
+    return ++count;
+  };
+};
+
+var count = makeCounter();
+count(); // 1
+count(); // 2
+count(); // 3
+count(); // 4
 
 
 
@@ -41,29 +64,28 @@ var callFriend = function(){
 
 
 /*
-  Write a function called makeCounter that makes the following code work properly.
+Write a function that accepts another function as it's first argument and returns a new function
+(which invokes the original function that was passed in) that can only ever be executed once.
+Once completed, add a second argument that allows the function to be invoked N number of times.
+After the function has been called N number of times, console.log('STAHHP');
 */
 
-  //Code Here
-  var count = makeCounter();
-  count() // 1
-  count() // 2
-  count() // 3
-  count() // 4
+var onlyOnce = function(cb) {
+  var hasRun = false;
+  return function() {
+    if (!hasRun) {
+      hasRun = true;
+      return cb();
+    }
+  };
+};
 
-
-
-//Next Problem
-
-
-
-/*
-  Write a function that accepts another function as it's first argument and returns a new function
-  (which invokes the original function that was passed in) that can only ever be executed once.
-  Once completed, add a second argument that allows the function to be invoked N number of times.
-  After the function has been called N number of times, console.log('STAHHP');
-*/
-
-
-
-
+var onlyOnce = function(cb, n) {
+  var count = 0;
+  return function() {
+    if (count < n) {
+      count++;
+      return cb();
+    }
+  };
+};
